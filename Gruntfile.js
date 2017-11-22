8c604dae1db696f0be79ed33cc8454268be85da5
Jacek Wilczynski <jacekwilczynski5@gmail.com>
2017-11-22 09:54:39 +0100

    initial

---
+++ Gruntfile.js
@@ -0,0 +1,31 @@
+module.exports = function (grunt) {
+	grunt.initConfig({
+		sass: {
+			options: {
+				sourceMap: true,
+			},
+			dist: {
+				files: {
+					'css/main.css': 'scss/main.scss',
+				},
+			},
+		},
+		watch: {
+			scripts: {
+				files: ['scss/*.scss'],
+				tasks: ['sass'],
+				options: {
+					spawn: false,
+				},
+			},
+			configFiles: {
+				files: ['Gruntfile.js'],
+			},
+		},
+	});
+
+	grunt.loadNpmTasks('grunt-sass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+
+	grunt.registerTask('default', ['sass', 'watch']);
+}
